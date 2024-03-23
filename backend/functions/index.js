aa79d3c181399383ae91f32ab0cc9c9e486a4202
Hridyansh Dugar <dugar@ualberta.ca>
2024-03-23 16:32:44 -0600

    deploying

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -7,8 +7,19 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
+const express = require("express")
+const cors = require("cors")
+
+const app = express();
+app.use(cors());
+
+app.get("/test", (req, res)=>{
+    res.send("Aggi was here")
+})
+
+exports.app = functions.https.onRequest(app);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
