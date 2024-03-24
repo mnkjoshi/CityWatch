4f52dee850795c3aad1c82fcf888c26826d96738
PrathamArora20 <apratham2421@gmail.com>
2024-03-23 18:01:35 -0600

    Sidebar Stuff

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,65 +1,89 @@
+"use client";
 import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "./layout.module.css";
 import Link from "next/link";
 import "./globals.css";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
-export const metadata = {
-  title: "CitiWatch",
-  description: "CitiWatch",
-};
-
-const missinaSans = localFont({ src: "./MessinaSans.ttf", weight: "" })
-
+const missinaSans = localFont({ src: "./MessinaSans.ttf", weight: "" });
 export default function RootLayout({ children }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  console.log("Current path: ", pathname);
+
   return (
     <html lang="en">
-        <body className={`${styles.viewpage} ${missinaSans.className}`}>
-
-          <div className={styles.sidebar}>
-          
-          
-            {/* Navbar */}
-            <div className={styles.navbar}>
-              <Image
-                className={styles.logo}
-                src="/logo.svg"
-                alt="CitiWatch"
-                width={30}
-                height={30}
-              />
-              <h2 className={styles.title}>CitiWatch</h2>
-            </div>
-
-
-            {/* Sidebar Links*/}
-            <p className={styles.categoryLabel}>
-              <Image height={18} width={18} alt="analytics" src="/verticallines.svg" className={styles.iconLabel}/>
-              <Link href="/" className={styles.subSectionLabel}>
-                Home
-              </Link>
-            </p>
-
-            <p className={styles.categoryLabel}>
-              <Image height={18} width={18} alt="analytics" src="/verticallines.svg" className={styles.iconLabel}/>
-              <Link href="/report" className={styles.subSectionLabel}>
-                Report
-              </Link>
-            </p>
-
-            <p className={styles.categoryLabel}>
-              <Image height={18} width={18} alt="analytics" src="/verticallines.svg" className={styles.iconLabel}/>
-              <Link href="/find" className={styles.subSectionLabel}>
-                Find
-              </Link>
-            </p>
+      <body className={`${styles.viewpage} ${missinaSans.className}`}>
+        <div className={styles.sidebar}>
+          {/* Navbar */}
+          <div className={styles.navbar}>
+            <Image
+              className={styles.logo}
+              src="/logo.svg"
+              alt="CitiWatch"
+              width={30}
+              height={30}
+            />
+            <h2 className={styles.title}>CitiWatch</h2>
           </div>
 
-          <div className={styles.mainApp}>
-            {children}
-          </div>
-          
-        </body>
+          {/* Sidebar Links*/}
+          <p
+            className={`${styles.categoryLabel} ${
+              pathname === "/" ? styles.active : ""
+            }`}
+          >
+            <Image
+              height={18}
+              width={18}
+              alt="analytics"
+              src="/verticallines.svg"
+              className={styles.iconLabel}
+            />
+            <Link href="/" className={styles.subSectionLabel}>
+              Home
+            </Link>
+          </p>
+
+          <p
+            className={`${styles.categoryLabel} ${
+              pathname.startsWith("/report") ? styles.active : ""
+            }`}
+          >
+            <Image
+              height={18}
+              width={18}
+              alt="analytics"
+              src="/verticallines.svg"
+              className={styles.iconLabel}
+            />
+            <Link href="/report" className={styles.subSectionLabel}>
+              Report
+            </Link>
+          </p>
+
+          <p
+            className={`${styles.categoryLabel} ${
+              pathname.startsWith("/find") ? styles.active : ""
+            }`}
+          >
+            <Image
+              height={18}
+              width={18}
+              alt="analytics"
+              src="/verticallines.svg"
+              className={styles.iconLabel}
+            />
+            <Link href="/find" className={styles.subSectionLabel}>
+              Find
+            </Link>
+          </p>
+        </div>
+
+        <div className={styles.mainApp}>{children}</div>
+      </body>
     </html>
   );
 }
